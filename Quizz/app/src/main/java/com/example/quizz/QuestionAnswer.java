d2bf6517d4aec525c73c9171ae9f916b5f284ebc
Sanjiv23110 <117466745+Sanjiv23110@users.noreply.github.com>
2023-04-13 23:26:47 +0530

    Quizz

---
+++ Quizz/app/src/main/java/com/example/quizz/QuestionAnswer.java
@@ -0,0 +1,29 @@
+package com.example.quizz;
+
+public class QuestionAnswer {
+    public static String question[]={
+            "What is the hottest planet in the solar system?",
+            "What's the largest bone in the human body?",
+            "In the comics, who’s the first avenger to quit the team?",
+            "After the cancellation of his first series, which comic did the Hulk make regular appearances in?",
+            "Who did The Winter Soldier take shelter with during “Infinity War”?",
+
+    };
+    public static String choices[][]={
+            {"Earth","Mars","Jupiter","Venus"},
+            {"sternum","ribs","Femur","cervical"},
+            {"Black Panther","Hulk","Black Widow","Captain America"},
+            {"Tales to Astonish","Damage Control","Nextwave: Agents of H.A.T.E.","The Immortal Iron Fist"},
+            {"Hawkeye","Winter Solider","Ant-Man","Black Panther"}
+
+    };
+    public static String correctAnswers[]={
+            "Venus",
+            "Femur",
+            "Hulk",
+            "Tales to Astonish",
+            "Black Panther"
+
+
+    };
+}
